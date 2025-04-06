f2e9ee097caa8d5547ec7120b3f3e39f9611fdb5
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-06 08:29:50 -0300

    fix: fix web server for deploy

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from "express";
+import express, { type Request, type Response } from "express";
 
 const app = express();
 
@@ -8,6 +8,8 @@ app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server is running on http://localhost:${PORT}`);
+// });
+
+export default app;
