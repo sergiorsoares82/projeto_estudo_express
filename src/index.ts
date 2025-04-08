039785964eea64433ff332038a4f512ab4514c7d
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-08 07:41:12 -0300

    feat: adds `listen` in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,20 +1,23 @@
 import express, { type Request, type Response } from "express";
+import dotenv from "dotenv";
+import { existsSync } from "node:fs";
 
 const app = express();
 
 const PORT = 3001;
+const envFile = `.env.${process.env.NODE_ENV}`;
+if (existsSync(envFile)) {
+  dotenv.config({ path: envFile });
+} else {
+  dotenv.config(); // fallback para .env
+}
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-// if (process.env.NODE_ENV === "development") {
 app.listen(PORT, () => {
   console.log(`Server is running on http://localhost:${PORT}`);
 });
-// }
-// app.listen(PORT, () => {
-//   console.log(`Server is running on http://localhost:${PORT}`);
-// });
 
 export default app;
