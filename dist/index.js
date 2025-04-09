59413a170936c8c7499aac6b2db8a9c48bd4f07f
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-09 06:55:17 -0300

    fix: changes `tsconfig.json` to build modules correctly

---
+++ dist/index.js
@@ -0,0 +1,21 @@
+import express from "express";
+const app = express();
+// app.use(cors());
+app.use(express.json());
+const PORT = 3001;
+const envFile = `.env.${process.env.NODE_ENV}`;
+// if (existsSync(envFile)) {
+//   dotenv.config({ path: envFile });
+// } else {
+//   dotenv.config(); // fallback para .env
+// }
+app.get("/", (req, res) => {
+    res.send("Hello World!");
+});
+app.use((req, res) => {
+    res.status(404).json({ error: "Route not found" });
+});
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
+export default app;
