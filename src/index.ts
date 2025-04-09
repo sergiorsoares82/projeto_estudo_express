6dbdf5d62ccf161cb9d91352e053c28f1691b493
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-08 21:02:45 -0300

    feat: adds 404 error

--- src/index.ts
+++ src/index.ts
@@ -10,14 +10,18 @@ app.use(express.json());
 
 const PORT = 3001;
 const envFile = `.env.${process.env.NODE_ENV}`;
-if (existsSync(envFile)) {
-  dotenv.config({ path: envFile });
-} else {
-  dotenv.config(); // fallback para .env
-}
+// if (existsSync(envFile)) {
+//   dotenv.config({ path: envFile });
+// } else {
+//   dotenv.config(); // fallback para .env
+// }
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
+});
+
+app.use((req, res) => {
+  res.status(404).json({ error: "Route not found" });
 });
 
 app.listen(PORT, () => {
