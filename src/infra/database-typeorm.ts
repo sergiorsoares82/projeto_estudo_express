00921b24654e0fc8d7f16c7bdc164325c92801d6
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-22 11:47:05 -0300

    handles database ssl connection

--- src/infra/database-typeorm.ts
+++ src/infra/database-typeorm.ts
@@ -3,7 +3,7 @@ import { DataSource } from "typeorm";
 export const AppDataSource = new DataSource({
   type: "postgres",
   host: process.env.TYPEORM_HOST,
-  port: parseInt(process.env.TYPEORM_PORT || "5432"),
+  port: parseInt(process.env.TYPEORM_PORT || "5436"),
   username: process.env.TYPEORM_USER,
   password: process.env.TYPEORM_PASSWORD,
   database: process.env.TYPEORM_DATABASE,
