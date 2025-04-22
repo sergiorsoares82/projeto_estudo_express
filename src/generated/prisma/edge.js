7b000501ff73212f7e6d9186be7973fb92b7f72d
Sergio Soares <sergio.veiga.soares@gmail.com>
2025-04-22 10:39:25 -0300

    fix: tries to solve prisma connection adding .env.production

--- src/generated/prisma/edge.js
+++ src/generated/prisma/edge.js
@@ -131,7 +131,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
--- src/generated/prisma/index.js
+++ src/generated/prisma/index.js
@@ -132,7 +132,7 @@ const config = {
     "isCustomOutput": true
   },
   "relativeEnvPaths": {
-    "rootEnvPath": null,
+    "rootEnvPath": "../../../.env",
     "schemaEnvPath": "../../../.env"
   },
   "relativePath": "../../../prisma",
